af2707f58241ec9c78366fb5eeb69020c40c2c92
rodionbgd <rodionbgd@mail.ru>
2021-08-13 19:28:57 +0300

    Тестирование 8 блока.

---
+++ src/08_homework/8.js
@@ -0,0 +1,57 @@
+const getDay = () => {
+  const dateStr = prompt('Enter date DD.MM.YYYY', '');
+  const dateArr = dateStr.split('.').reverse();
+  if (dateArr.length !== 3) {
+    throw new Error('Arg != 3');
+  }
+  dateArr.map(value => {
+    if (isNaN(+value) || +value <= 0) {
+      throw new Error(`Invalid value: ${value}`);
+    }
+  });
+  const date = new Date(dateArr);
+  const day = date.toLocaleDateString('en-US', { weekday: 'long' });
+  console.log(day);
+};
+
+const getMinutes = () => {
+  const date = new Date();
+  console.log(date.getHours() * 60 + date.getMinutes());
+};
+
+function checkValue(value) {
+  if (isNaN(+value) || +value <= 0) {
+    throw new Error(`Invalid value: ${value}`);
+  }
+}
+
+const isOlder = (person1, person2) => {
+  if (typeof person1 === 'undefined' || typeof person2 === 'undefined')
+    throw new Error('Args < 2');
+  const date1 = person1.split('.').reverse();
+  const date2 = person2.split('.').reverse();
+  if (date1.length !== 3 || date2.length !== 3) {
+    throw new Error('Arg != 3');
+  }
+  try {
+    date1.map(checkValue);
+  } catch (e) {
+    throw e;
+  }
+  try {
+    date2.map(checkValue);
+  } catch (e) {
+    throw e;
+  }
+  let date = new Date(date1);
+  if (date.toString() === 'Invalid Date') {
+    throw new Error('Invalid Date');
+  }
+  date = new Date(date2);
+  if (date.toString() === 'Invalid Date') {
+    throw new Error('Invalid Date');
+  }
+  return date1.join('.') < date2.join('.');
+};
+
+module.exports = { getDay, getMinutes, isOlder };
---
+++ src/08_homework/8.test.js
@@ -0,0 +1,143 @@
+/**
+ * @jest-environment jsdom
+ */
+const { describe, test, beforeEach, afterEach } = require('@jest/globals');
+const { getDay, getMinutes, isOlder } = require('./8');
+
+describe('Get day by date', () => {
+  let prompt;
+  let originalPrompt;
+  const originalConsoleLog = console.log;
+  beforeEach(() => {
+    console.log = jest.fn();
+    originalPrompt = window.prompt;
+    prompt = jest.spyOn(window, 'prompt');
+  });
+  afterEach(() => {
+    window.prompt = originalPrompt;
+  });
+  afterAll(() => {
+    console.log = originalConsoleLog;
+  });
+  test('Invalid number of args', () => {
+    const values = ['1.2', '1', '1.2.3.4', '1,2,3'];
+    values.map(value => {
+      try {
+        prompt.mockImplementationOnce(() => value);
+      } catch (e) {
+        expect(getDay()).toThrow('Arg != 3');
+      }
+    });
+  });
+  test('Invalid value of arg', () => {
+    const values = [
+      ['11.a.2021', 'Invalid value: -a'],
+      ['-11.11.2021', 'Invalid value: -11'],
+    ];
+    values.map(value => {
+      try {
+        prompt.mockImplementationOnce(() => value[0]);
+      } catch (e) {
+        expect(getDay()).toThrow(value[1]);
+      }
+    });
+  });
+  test('Invalid date', () => {
+    const values = ['11.33.2021', '221.11.2021'];
+    values.map(value => {
+      const log = jest.spyOn(console, 'log');
+      prompt.mockImplementationOnce(() => value);
+      getDay();
+      expect(log).toHaveBeenCalledWith('Invalid Date');
+    });
+  });
+  test('Correct date', () => {
+    const values = [
+      ['08.09.2021', 'Wednesday'],
+      ['01.08.2021', 'Sunday'],
+    ];
+    values.map(value => {
+      prompt.mockImplementationOnce(() => value[0]);
+      getDay();
+      expect(console.log).toHaveBeenCalledWith(value[1]);
+    });
+  });
+});
+
+describe('Get minutes of day', () => {
+  test('Get minutes', () => {
+    for (let hour = 0; hour < 10; hour++) {
+      for (let min = 0; min < 10; min++) {
+        const log = jest.spyOn(console, 'log');
+        jest
+          .useFakeTimers('modern')
+          .setSystemTime(new Date(`2021-08-08T0${hour}:0${min}:00`));
+        getMinutes();
+        expect(log).toHaveBeenCalledWith(hour * 60 + min);
+      }
+    }
+  });
+});
+
+describe('Who is older', () => {
+  test('Few args', () => {
+    const values = ['1.2.3', '1.2'];
+    values.map(value => {
+      try {
+        isOlder(value);
+      } catch (e) {
+        expect(e.message).toBe('Args < 2');
+      }
+    });
+  });
+  test('Invalid number of args', () => {
+    const values = [
+      ['1.2.2021', '1.2'],
+      ['1.2', '1.2.2021'],
+      ['1.2.3.4', '1.2'],
+      ['1,2,3', '1.2.2021'],
+    ];
+    values.map(value => {
+      try {
+        isOlder(value[0], value[1]);
+      } catch (e) {
+        expect(e.message).toBe('Arg != 3');
+      }
+    });
+  });
+  test('Invalid value of arg', () => {
+    const values = [
+      ['11.a.2021', '11.1.2021', 'Invalid value: a'],
+      ['-11.11.2021', '1.2.2021', 'Invalid value: -11'],
+    ];
+    values.map(value => {
+      try {
+        isOlder(value[0], value[1]);
+      } catch (e) {
+        expect(e.message).toBe(value[2]);
+      }
+    });
+  });
+  test('Invalid date', () => {
+    const values = [
+      ['11.33.2021', '1.2.2021'],
+      ['221.11.2021', '1.2.2021'],
+    ];
+    values.map(value => {
+      try {
+        isOlder(value[0], value[1]);
+      } catch (e) {
+        expect(e.message).toBe('Invalid Date');
+      }
+    });
+  });
+  test('Correct dates', () => {
+    const values = [
+      ['08.08.2021', '1.12.2021', true],
+      ['09.09.2021', '08.08.2020', false],
+    ];
+    values.map(value => {
+      expect(isOlder(value[0], value[1])).toBe(value[2]);
+    });
+  });
+});
